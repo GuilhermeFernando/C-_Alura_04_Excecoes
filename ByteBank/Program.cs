c1a61bba56453b966ac4bb9f5e5336ac79ac3a4f
Guilherme Machado <guifermach@hotmail.com>
2022-05-02 21:30:13 -0300

    Projeto ByteBank Excecoes concluido

---
+++ ByteBank/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cath no metodo MAIN");
+            }
+
+
+            //Metodo();
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        //-------------------------------------------------------
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+
+                leitor.LerProximaLinha();
+            }
+
+        //    LeitorDeArquivo leitor = null;
+        //    try
+        //    {
+        //        leitor = new LeitorDeArquivo("contas.txt");
+
+        //        leitor.LerProximaLinha();
+        //        leitor.LerProximaLinha();
+        //        leitor.LerProximaLinha();
+        //        leitor.LerProximaLinha();
+
+        //    }
+
+        //    finally
+        //    {
+        //        Console.WriteLine("Executando o finally");
+        //        if (leitor != null)
+        //        {
+        //            leitor.Fechar();
+        //        }
+        //    }
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(456, 4578420);
+                ContaCorrente conta2 = new ContaCorrente(485, 456478);
+
+                conta2.Transferir(-10, conta);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(-500);
+                Console.WriteLine(conta.Saldo);
+            }
+            catch (ArgumentException ex)
+            {
+                if (ex.ParamName == "numero")
+                {
+
+                }
+
+                Console.WriteLine("Argumento com problema: " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
+        }
+
+    }
+}
